ed5d362503de9906d2c1a50395f281d1a8d12102
Bobby Ren <bobbyren@gmail.com>
2015-06-11 21:24:42 -0400

    Tutorial 1: tap gestures

--- BRGestureTutorial/ViewController.swift
+++ BRGestureTutorial/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap = UITapGestureRecognizer(target: self, action: "handleGesture:")
+        self.view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func handleGesture(gesture: UIGestureRecognizer) {
+        let location = gesture.locationInView(self.view)
+        println("A gesture at \(location)")
+        
+        for view in self.view.subviews {
+            if CGRectContainsPoint(view.frame, location) {
+                println("view number \(view.tag)")
+            }
+        }
+    }
 }
 
